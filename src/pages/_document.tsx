1ed67ca71d887bf88df8e77479ca90b2f70385a1
Sumin Kim <zzmzzm0065@gmail.com>
2024-12-01 21:42:38 +0900

    chore: integrate google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -27,7 +27,10 @@ class MyDocument extends Document {
                 name="google-site-verification"
                 content={CONFIG.googleSearchConsole.config.siteVerification}
               />
-              <meta name="google-site-verification" content="mH9jBCopmCRxCYk8MakLBP8nZ8W6Pxp_jwG7LxCAxIQ" />
+              <meta
+                name="google-site-verification"
+                content="mH9jBCopmCRxCYk8MakLBP8nZ8W6Pxp_jwG7LxCAxIQ"
+              />
             </>
           )}
           {/* naver search advisor */}
@@ -39,8 +42,25 @@ class MyDocument extends Document {
               />
             </>
           )}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer',${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID});`,
+            }}
+          />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
